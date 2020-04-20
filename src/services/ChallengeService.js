aa24a00001d5f2ecd352851df0d4a96b6f866f46
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-04-20 10:36:22 +0100

    Added challenge title

--- src/services/ChallengeService.js
+++ src/services/ChallengeService.js
@@ -4,3 +4,4 @@ export const all = () => getFunc(`challenges`);
 export const create = (payload) => postFunc(`challenge/create`, payload);
 export const update = (id, payload) => putFunc(`challenge/${id}`, payload);
 export const view = (id) => getFunc(`challenge/${id}`);
+export const current = () => getFunc(`challenge/active`);
